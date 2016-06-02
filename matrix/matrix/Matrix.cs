77f0ce12520073abb7f31a6ba2eacde973475148
Murddraal <2Dents@mail.ru>
2016-06-02 10:03:29 +0600

    2

--- matrix/matrix/Matrix.cs
+++ matrix/matrix/Matrix.cs
@@ -7,69 +7,131 @@ using System.Threading.Tasks;
 
 
 namespace matrix
-{
-    
+{    
 
     class Matrix
     {
-        private List<List<double>> _matrix=new List<List<double>>();
-        private int height, width;
+        //Операции, с которыми работает класс Matrix
+        public static string[]  operations = new string[] { "Sum", "Mult", "MultMatrix" };
 
-        public int Width
+        private List<List<double>> _matrix=new List<List<double>>();
+
+        private int _height, _width;
+
+        public Matrix() { }
+
+        public Matrix(int height, int width)
         {
-            get { return width; }
+            _height = height; _width = width;
+            for (int i = 0; i < height; ++i)
+                _matrix.Add(new List<double>());
+            foreach (var raw in _matrix)
+                for (int i = 0; i < width; ++i)
+                    raw.Add(0);
+        }
+
+        public Matrix(List<List<double>> matrix)
+        {
+            _matrix = matrix;
+            _height = matrix.Count;
+            _width = matrix[0].Count;
+        }
+
+        public static List<List<double>> Read(int height, int width)
+        {
+            //создание массива, куда будет поступать информация
+            List<List<double>> matrix = new List<List<double>>();
+            for (int i = 0; i < height; i++)
+                matrix.Add(new List<double>());
+
+            bool flag = false;
+            //цикл осуществляется до тех пор, пока матрица не будет введена корректно
+            while (!flag)
+            {
+                for (int i = 0; i < height; i++)
+                {
+                    string[] strLine = Console.ReadLine().Split('\t');
+                    //если длина строки оказалась меньше строки матрицы
+                    if (strLine.Length != width)
+                    {
+                        flag = false;
+                        Console.WriteLine("Введённые значения матрицы неверны. Введите матрицу заново:");
+                        break;
+                    }
+                    //проверяются считанные значения на корректность
+                    double[] line = new double[width];
+                    for (int j = 0; j < width; j++)
+                        if (!(flag = double.TryParse(strLine[j], out line[j])))
+                            break;
+                    if (!flag)
+                    {
+                        Console.WriteLine("Введённые значения матрицы неверны. Введите матрицу заново:");
+                        break;
+                    }
+                    //строка из массива double преобразуется в список double и идёт в матрицу
+                    matrix[i] = line.ToList<double>();
+                }
+            }
+            return matrix;
         }
 
         public int Height
         {
-            get{ return height; }
+            get { return _height; }
         }
 
-        public List<List<double>>Elements
+        public int Width
+        {
+            get { return _width; }
+        }
+
+        public List<List<double>> Elements
         {
             get { return _matrix; }
         }
 
-        public Matrix(int n, int m)
-        {
-            height = n; width = m;
-            for (int i = 0; i < n; ++i)
-                _matrix.Add(new List<double>());
-            foreach (var raw in _matrix)
-                for (int i = 0; i < m; ++i)
-                    raw.Add(0);
-        }
-
-        public void Read(string strMatrix)
-        {
-        }
-
-
         public static Matrix operator *( Matrix A, Matrix B)
         {
             Matrix result = new Matrix(A.Height, B.Width);
-            for(int i=0; i< result.Height; i++)
-                for(int j=0; j< i; j++)
+            //создаётся матрица, являющаяся транспонированной матрицей B
+            //умножение матрицы на транспонированную матрицу происходит быстрее, нежели на обычную, т.к. идёт перебор только по стобцам 
+            #region Транспонирование
+            var matrBuf = new List<List<double>>();
+            for (int i = 0; i < B.Width; ++i)
+            {
+                matrBuf.Add(new List<double>());
+                for (int j = 0; j < B.Height; ++j)
+                    matrBuf[i].Add(B.Elements[j][i]);
+            }                
+
+            for(int i=0; i< B.Height; i++)
+                for(int j=0; j<= i; j++)
                 {
-                    var buf = B.Elements[i][j];
-                    B._matrix[i][j] = B._matrix[j][i];
-                    B._matrix[j][i] = buf;
+                    matrBuf[j][i] = B.Elements[i][j];
+                    matrBuf[i][j] = B.Elements[j][i];
                 }
+            var buf = new Matrix(matrBuf);
+            #endregion
 
             for (int i = 0; i < result.Height; i++)
                 for (int j = 0; j < result.Width; j++)
                     for (int k = 0; k < A.Width; k++)
-                        result._matrix[i][j] += A._matrix[i][k] * B._matrix[j][k];
-            return A;
+                        result._matrix[i][j] += A.Elements[i][k] * buf.Elements[j][k];
+            return result;
         }
 
         public static Matrix operator *(Matrix A, double x)
         {
+               
+            return x*A;
+        }
+
+        public static Matrix operator *(double x, Matrix A)
+        {
             Matrix result = A;
-            foreach (var raw in result._matrix)
-                for (int i = 0; i < result.Width; i++)
-                    raw[i] *= x;
-            
+            for (int i = 0; i < result.Height; i++)
+                for (int j = 0; j < result.Width; j++)
+                    result._matrix[i][j] *= x;
             return result;
         }
 
--- matrix/matrix/Program.cs
+++ matrix/matrix/Program.cs
@@ -11,6 +11,132 @@ namespace matrix
         static void Main(string[] args)
         {
 
+            string operation;
+            Console.WriteLine("Данная программа реализует следующие операции над матрицами: Sum, Mult, MultMatrix.");
+            Console.WriteLine("Все вводимые числа дожны отедляться друг от друга табуляцией.");
+            Console.WriteLine("Введите тип операции (Sum, Mult, MultMatrix):");
+
+            //проверка операции на корректность. цикл работает до получения корректных данных
+            while (true)
+                if (!Matrix.operations.Contains(operation = Console.ReadLine()))
+                    Console.WriteLine("Операция введена неверно! Попробуйте снова");
+                else
+                    break;
+
+            var result=new Matrix();
+
+            switch (operation)
+            {
+                case "Sum":
+                    {
+                        Console.WriteLine("Введите размерность матриц одной строкой:");
+                        int height = 0, width = 0;
+                        while (true)
+                        {
+                            string[] strSize = Console.ReadLine().Split('\t');
+                            //считываем строку, содеражащую высоту и ширину матрицы. Строка разбивается на подстроки, разделитель - \t. 
+                            //Если данные некорректны, то осуществляется повторный ввод данных
+                            if (strSize.Length != 2)
+                            {
+                                Console.WriteLine("Неверное кол-во параметров! Попробуйте снова");
+                                continue;
+                            }
+                            if (!(int.TryParse(strSize[0], out height) && int.TryParse(strSize[1], out width)) || !(height > 0 && width > 0))
+                                Console.WriteLine("Размерность введена неверно! Попробуйте снова:");                            
+                            else
+                                break;
+                        }
+
+                        Console.WriteLine("Введите первую матрицу:");
+
+                        var matrixA = new Matrix(Matrix.Read(height, width));
+
+                        Console.WriteLine("Ввелите вторую матрицу:");
+
+                        var matrixB = new Matrix(Matrix.Read(height, width));
+
+                        result = matrixA + matrixB;
+                        break;
+                    }
+                case "Mult":
+                    {
+                        Console.WriteLine("Введите размерность матрицы:");
+                        int height = 0, width = 0;
+                        while (true)
+                        {
+                            var strSize = Console.ReadLine().Split('\t');
+
+                            if (strSize.Length != 2)
+                            {
+                                Console.WriteLine("Неверное кол-во параметров! Попробуйте снова");
+                                continue;
+                            }
+                            if (!(int.TryParse(strSize[0], out height) && int.TryParse(strSize[1], out width))|| !(height > 0 && width > 0))
+                                Console.WriteLine("Размерность введена неверно! Попробуйте снова");                            
+                            else
+                                break;
+                        }
+
+                        Console.WriteLine("Ввелите матрицу:");
+
+                        var matrix = new Matrix(Matrix.Read(height, width));
+
+                        Console.WriteLine("Ввелите число, на которое хотите умножить матрицу:");
+                        bool flag = false;
+                        double numb = 0;
+
+                        while (!flag)
+                        {
+                            string strNumb;
+                            if (!(flag = double.TryParse(strNumb = Console.ReadLine(), out numb)))
+                                Console.WriteLine("Число введено неверно! Попробуйте снова");
+                            else
+                                break;
+                        }
+
+                        result = matrix * numb;
+                        break;
+                    }
+                case "MultMatrix":
+                    {
+                        Console.WriteLine("Введите три числа, задающих размерность матриц(второе число является шириной первой матрицы и высотой второй):");
+                        int heightA = 0, widthHeight = 0, widthB = 0;
+
+                        while (true)
+                        {
+                            var strSize = Console.ReadLine().Split('\t');
+                            if (strSize.Length != 3)
+                            {
+                                Console.WriteLine("Неверное кол-во параметров! Попробуйте снова");
+                                continue;
+                            }
+                            //считываем строку, сожеражащую высоту и ширину матрицы. Строка разбивается на подстроки, разделитель - \t. 
+                            //Если данные некорректны, то нужно ввест
+                            if (!(int.TryParse(strSize[0], out heightA) && int.TryParse(strSize[1], out widthHeight) && int.TryParse(strSize[2], out widthB)) || !(heightA > 0 && widthB > 0 && widthHeight > 0))
+                                Console.WriteLine("Размерность введена неверно! Попробуйте снова:");                            
+                            else
+                                break;
+                        }                       
+
+                        Console.WriteLine("Ввелите первую матрицу:");
+                        var matrixA = new Matrix(Matrix.Read(heightA, widthHeight));
+
+                        Console.WriteLine("Ввелите вторую матрицу:");
+                        var matrixB = new Matrix(Matrix.Read(widthHeight, widthB));
+
+                        result = matrixA * matrixB;
+                        break;
+                    }
+            }
+
+            Console.WriteLine("Результат:");
+
+            for (int i = 0; i < result.Height; ++i)
+            {
+                for (int j = 0; j < result.Width; ++j)
+                    Console.Write(result.Elements[i][j].ToString() + '\t');
+                Console.WriteLine();
+            }
         }
     }
 }
